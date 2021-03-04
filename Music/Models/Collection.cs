bd1e23fe64c24b1d7dc8dbc677167ac7972a8b21
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-04 13:35:24 -0800

    create a collection file
    Co-authored-by: Ash Porter <porter.ashley13@gmail.com>

--- Music/Models/Collection.cs
+++ Music/Models/Collection.cs
@@ -4,6 +4,20 @@ namespace Music.Models
 {
   public class Collection
   {
-    
+    private static List<Collection> _instances = new List<Collection> {};
+    public string Type { get; set; }
+    public int Id { get; }
+    public string Artist { get; set; }
+    public Collection(string type, string artist)
+    {
+      Type = type;
+      Artist = artist;
+      Id = _instances.Count;
+      _instances.Add(this);
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
